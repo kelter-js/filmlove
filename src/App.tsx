acbac91d067d2ed4001775b2d7f62514ddf78336
kelter-js <spawnkillerqw@gmail.com>
2025-05-29 15:48:20 +0500

    feat: main rendering files

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { useGetTrendingMovies } from "./hooks/useGetTrendingMovies";
+import { MovieList } from "./components/MovieList";
+import { useDebounce } from "./hooks/useDebounce";
+import { useGetFilms } from "./hooks/useGetFilms";
+import { Search } from "./components/Search";
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const debouncedSearchTerm = useDebounce(searchTerm);
+
+  const { errorMessage, movieList, isLoading } =
+    useGetFilms(debouncedSearchTerm);
+
+  const trendingMovies = useGetTrendingMovies();
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <main className="main-container">
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You`ll Enjoy
+            Without the Hassle
+          </h1>
 
-export default App
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+
+        {trendingMovies.length > 0 && (
+          <section className="trending">
+            <h2>Trending Movies</h2>
+
+            <ul>
+              {trendingMovies.map((movie, index) => (
+                <li key={movie.$id}>
+                  <p>{index + 1}</p>
+                  <img src={movie.poster_url} alt={movie.title} />
+                </li>
+              ))}
+            </ul>
+          </section>
+        )}
+
+        <section className="all-movies">
+          <h2>All Movies</h2>
+
+          <MovieList
+            isLoading={isLoading}
+            errorMessage={errorMessage}
+            movies={movieList || []}
+          />
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default App;
